d83d0ff58b003f7c97b4aee1fa568f7d2d304088
Faiz Kidwai <f.y.kidwai@gmail.com>
2020-11-23 22:29:55 +0530

    adding send and receive example

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgjms.listener;
 
+import java.util.UUID;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,16 +14,27 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listener(@Payload final HelloWorldMessage helloWorldMessage, @Headers final MessageHeaders messageHeaders,
         final Message message) {
-        System.out.println("I got a message..!!!");
+//        System.out.println("I got a message..!!!");
 
-        System.out.println(helloWorldMessage);
+//        System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload final HelloWorldMessage helloWorldMessage,
+        @Headers final MessageHeaders messageHeaders, final Message message) throws JMSException {
+        final var payloadMsg = HelloWorldMessage.builder().id(UUID.randomUUID()).message("World").build();
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 
 }
--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -2,9 +2,15 @@ package guru.springframework.sfgjms.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
@@ -15,15 +21,34 @@ import lombok.RequiredArgsConstructor;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
-        System.out.println("I'm sending a message");
 
         final var message = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello World..!!").build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-        System.out.println("Message sent");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReiceveMessage() throws JMSException {
+
+        final var message = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello").build();
+
+        Message recievedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, session -> {
+            Message helloMessage = null;
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+                System.out.println("Sending Hello");
+                return helloMessage;
+            } catch (JsonProcessingException | JMSException e) {
+                throw new JMSException("boom");
+            }
+        });
+
+        System.out.println(recievedMsg.getBody(String.class));
     }
 }
